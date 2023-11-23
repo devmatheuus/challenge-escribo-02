53b13879af4c3ded92ccf397abfd886751fc36c5
Matheus Silva <matheus_lsilva@hotmail.com>
2023-11-23 10:36:08 -0300

    feat: add global middleware to handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'express-async-errors';
+import 'module-alias/register';
 import express from 'express';
+import { appRoutes } from './routes/index.routes';
+import { globalErrorMiddleware } from './middlewares/global-error.middleware';
 
 const app = express();
-app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('It works!');
-});
+app.use(express.json());
+appRoutes(app);
+app.use(globalErrorMiddleware);
 
 export default app;
---
+++ src/middlewares/global-error.middleware.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@/errors/app-error';
+import { z } from 'zod';
+
+type ZodErrorType = {
+    code: string;
+    expected: string;
+    received: string;
+    path: string[];
+    message: string;
+};
+
+type ErrorMessageReturnType = {
+    [key: string]: string;
+};
+
+export const globalErrorMiddleware = async (
+    error: any,
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            message: error.message,
+        });
+    }
+
+    if (error instanceof z.ZodError) {
+        const message: ErrorMessageReturnType[] = [];
+
+        const errors: ZodErrorType[] = JSON.parse(error.message);
+
+        errors.forEach((err) => {
+            const errorObject = {
+                [err.path[0]]: err.message,
+            };
+
+            message.push(errorObject);
+        });
+
+        return response.status(400).json({
+            mensagem: message,
+        });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        message: 'Internal server error',
+    });
+};
