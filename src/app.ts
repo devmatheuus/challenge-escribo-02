e2b3dfbbf4fd1dd5afce147229004674bacca315
Matheus Silva <matheus_lsilva@hotmail.com>
2023-11-23 16:18:39 -0300

    feat: add cors package

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import 'module-alias/register';
 import express from 'express';
 import { appRoutes } from './routes/index.routes';
 import { globalErrorMiddleware } from './middlewares/global-error.middleware';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 appRoutes(app);
 app.use(globalErrorMiddleware);
 
