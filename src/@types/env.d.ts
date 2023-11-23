e4fc585a89b144e05ca29883cdc4dd1e9555c48f
Matheus Silva <matheus_lsilva@hotmail.com>
2023-11-23 16:00:54 -0300

    chore: add environment variable for test database name

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -4,6 +4,7 @@ declare global {
             POSTGRES_USER: string;
             POSTGRES_PASSWORD: string;
             POSTGRES_DB: string;
+            POSTGRES_DB_TEST: string;
             DB_HOST: string;
             SECRET_KEY: string;
             NODE_ENV: 'development' | 'production' | 'test';
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,26 +1,18 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === 'test'
-        ? {
-              type: 'sqlite',
-              database: ':memory:',
-              synchronize: true,
-              entities: ['src/entities/*.ts'],
-          }
-        : {
-              type: 'postgres',
-              host: process.env.DB_HOST,
-              port: 5432,
-              username: process.env.POSTGRES_USER,
-              password: process.env.POSTGRES_PASSWORD,
-              database: process.env.POSTGRES_DB,
-              synchronize: false,
-              logging: true,
-              entities: ['src/entities/*.ts'],
-              migrations: ['src/migrations/*.ts'],
-          }
-);
+const isTestEnvironment = process.env.NODE_ENV === 'test';
 
+const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: isTestEnvironment ? process.env.POSTGRES_DB_TEST : process.env.POSTGRES_DB,
+    synchronize: isTestEnvironment,
+    logging: false,
+    entities: ['src/entities/*.ts'],
+    migrations: ['src/migrations/*.ts'],
+});
 export default AppDataSource;
