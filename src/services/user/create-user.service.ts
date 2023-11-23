ed5a03acc4d8c522d3a14d6b636e32edc66d8b9e
Matheus Silva <matheus_lsilva@hotmail.com>
2023-11-23 14:29:21 -0300

    fix: saving phone numbers on database when user is being created

--- src/services/user/create-user.service.ts
+++ src/services/user/create-user.service.ts
@@ -1,4 +1,5 @@
 import AppDataSource from '@/data-source';
+import { Phone } from '@/entities/phone.entities';
 import { User } from '@/entities/user.entities';
 import { AppError } from '@/errors/app-error';
 import { CreateUserSchemaType } from '@/schemas/user/create-user.schema';
@@ -8,6 +9,7 @@ export const CreateUserService = async (user: CreateUserSchemaType) => {
     const { nome: name, email, senha: password, telefones: phones } = user;
 
     const userRepository = AppDataSource.getRepository(User);
+    const phoneRepository = AppDataSource.getRepository(Phone);
 
     const emailAlreadyExists = await userRepository.findOneBy({ email });
 
@@ -23,6 +25,14 @@ export const CreateUserService = async (user: CreateUserSchemaType) => {
         password: hashPassword,
         phones,
     });
+
+    for await (const phone of phones) {
+        await phoneRepository.save({
+            ddd: phone.ddd,
+            number: phone.numero,
+            user: newUser,
+        });
+    }
 
     const userResponse = {
         id: newUser.id,
